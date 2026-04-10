398ef90549090326dc6687fb0e0d2b0632eb3c8b
ddkurnia <kurnia200688@gmail.com>
2026-04-10 21:03:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 //   yang TIDAK AKAN terhapus saat cache diperbarui
 // ============================================
 
-const CACHE_NAME = 'warkop-pos-v8';
+const CACHE_NAME = 'warkop-pos-v9';
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
