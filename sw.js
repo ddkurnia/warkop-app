82b5c308feac915e74bb3dd4a2235b80212dc92a
ddkurnia <kurnia200688@gmail.com>
2026-04-11 17:37:26 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ============================================
-// Service Worker v13 - Warung Kopi POS
+// Service Worker v14 - Warung Kopi POS
 // ============================================
 // CATATAN PENTING:
 // - Service Worker ini TIDAK PERNAH menyentuh IndexedDB atau localStorage
@@ -8,7 +8,7 @@
 //   yang TIDAK AKAN terhapus saat cache diperbarui
 // ============================================
 
-const CACHE_NAME = 'warkop-pos-v17';
+const CACHE_NAME = 'warkop-pos-v18';
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
