917f47311bf7579766b9c15f8a95c003398af702
ddkurnia <kurnia200688@gmail.com>
2026-04-15 00:29:27 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
 // - Network-first strategy untuk reliability
 // ============================================
 
-const CACHE_NAME = 'warkop-pos-v21';
+const CACHE_NAME = 'warkop-pos-v24';
 const STATIC_ASSETS = [
   '/',
   '/manifest.json',
